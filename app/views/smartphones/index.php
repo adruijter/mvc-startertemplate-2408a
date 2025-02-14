a838d99f19055471ec839f64931c7c72cb70fbcb
adruijter <adruijter@gmail.com>
2025-02-14 15:32:00 +0100

    Uitgecommentarieerd

--- app/views/smartphones/index.php
+++ app/views/smartphones/index.php
@@ -36,7 +36,7 @@
                     <?php endforeach; ?>
                 </tbody>
             </table>
-            <a href="<?= URLROOT; ?>">Terug</a>
+            <!-- <a href="<?= URLROOT; ?>">Terug</a> -->
         </div>
         <div class="col-2"></div>
     </div>
