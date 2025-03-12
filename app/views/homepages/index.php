3cc23caaa1eaad7b403adcb52ce4918ba383e11c
adruijter <adruijter@gmail.com>
2025-03-12 16:04:40 +0100

    Laatste commit

--- app/views/homepages/index.php
+++ app/views/homepages/index.php
@@ -11,7 +11,8 @@
             <h3><?php echo $data['title']; ?></h3>
 
             <a href="<?= URLROOT; ?>/smartphones/index">Overzicht smartphones</a> |
-            <a href="<?= URLROOT; ?>/sneakers/index">Overzicht sneakers</a> 
+            <a href="<?= URLROOT; ?>/sneakers/index">Mooiste sneakers</a> | 
+            <a href="<?= URLROOT; ?>/horloges/index">Duurste Horloges</a> | 
 
         </div>
         
